87812c4bd8ef9ac42f786c0f46db54aedd55a6a4
Rockey C.R <rockeyrai234@gmail.com>
2025-03-09 08:03:51 +0545

    making a simple animation

--- myproject/src/main.js
+++ myproject/src/main.js
@@ -1,24 +1,30 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+const scene = new THREE.Scene
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth /window.innerHeight, 0.1,1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer({
+  canvas:document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth,window.innerHeight);
+camera.position.setZ(30)
+renderer.render(scene,camera)
+
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true})
+const torus = new THREE.Mesh(geometry,material);
+
+scene.add(torus)
+
+function animate(){
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+  torus.rotation.z += 0.01;
+  renderer.render(scene,camera)
+}
+
+animate()
